bc47788dda0a2911454954052c703be58f208168
Marcel <numax.cz@protonmail.ch>
2024-11-04 18:44:45 +0100

    Přidány ikony, přidán základ pro API service

---
+++ src/app/api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyDataService {
+  private readonly api_url: string = '';
+
+  constructor(private http: HttpClient) { }
+
+  public getData(): Observable<any> {
+    return this.http.get<any>(this.api_url);
+  }
+}
