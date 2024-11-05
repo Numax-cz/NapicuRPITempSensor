19382152f9e8c43925d0944d2705214658503a11
Marcel <numax.cz@protonmail.ch>
2024-11-05 15:50:53 +0100

    Upravena URL pro API

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,7 +7,7 @@ import {IApiData} from "./char";
   providedIn: 'root'
 })
 export class ApiDataService {
-  private readonly api_url: string = 'http://192.168.0.103:5000/api/temperature';
+  private readonly api_url: string = 'http://localhost:5000/api/temperature';
 
   constructor(private http: HttpClient) { }
 
